31433589c6a2954ca9fa2bd591f5a7d0d874af34
itsbasilsaman <itsbasilsaman@gmail.com>
2025-05-28 12:07:07 +0530

    Updation Phase

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,16 @@
-export function cn(...classes: (string | undefined | null | false)[]) {
-  return classes.filter(Boolean).join(' ')
+type ClassValue = string | number | boolean | undefined | null | { [key: string]: boolean | undefined | null }
+
+export function cn(...inputs: ClassValue[]) {
+  return inputs
+    .map((input) => {
+      if (typeof input === 'object' && input !== null) {
+        return Object.entries(input)
+          .filter(([_, value]) => Boolean(value))
+          .map(([key]) => key)
+          .join(' ')
+      }
+      return input
+    })
+    .filter(Boolean)
+    .join(' ')
 } 
